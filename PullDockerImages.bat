92dfedd2d90959371ff2c7ecf5a92220fd14d4db
Joslin.J <54833804+josliniyda27@users.noreply.github.com>
2022-11-15 17:54:27 +0530

    Update PullDockerImages.bat

--- PullDockerImages.bat
+++ PullDockerImages.bat
@@ -13,13 +13,6 @@ aws configure set region %AWS_Region%
 aws ecr get-login-password --region %AWS_Region% | docker login --username AWS --password-stdin %AWS_AccountID%.dkr.ecr.%AWS_Region%.amazonaws.com
 
 docker pull %AWS_AccountID%.dkr.ecr.%AWS_Region%.amazonaws.com/hdfc_ibpsportal:alpine-openjdk
-docker pull %AWS_AccountID%.dkr.ecr.%AWS_Region%.amazonaws.com/hdfc_ibps5serviceinstanceejb:sp2
-docker pull %AWS_AccountID%.dkr.ecr.%AWS_Region%.amazonaws.com/hdfc_ibps5serviceinstanceweb:sp2
-docker pull %AWS_AccountID%.dkr.ecr.%AWS_Region%.amazonaws.com/hdfc_ibps5userinstanceejb:sp2
-docker pull %AWS_AccountID%.dkr.ecr.%AWS_Region%.amazonaws.com/hdfc_ibps5userinstanceweb:sp2
-docker pull %AWS_AccountID%.dkr.ecr.%AWS_Region%.amazonaws.com/hdfc_ibpsnodeserver:alpine
-docker pull %AWS_AccountID%.dkr.ecr.%AWS_Region%.amazonaws.com/hdfc_od11.0services:ibps5.0sp2
-
 
 
 pause
