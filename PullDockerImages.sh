ff9cf5839a30c583c3d5091aafcba0f8f905dddb
Joslin.J <54833804+josliniyda27@users.noreply.github.com>
2022-11-16 12:07:39 +0530

    Update PullDockerImages.sh

--- PullDockerImages.sh
+++ PullDockerImages.sh
@@ -7,7 +7,7 @@ aws configure set aws_access_key_id $AWS_AccessKey
 aws configure set aws_secret_access_key $AWS_SecretKey
 aws configure set region $AWS_Region
 
-aws ecr get-login-password --region $AWS_Region | docker login --username AWS --password-stdin $AWS_AccountID$.dkr.ecr.$AWS_Region$.amazonaws.com
+aws ecr get-login-password --region $AWS_Region | docker login --username AWS --password-stdin $AWS_AccountID.dkr.ecr.$AWS_Region.amazonaws.com
 
 docker pull $AWS_AccountID.dkr.ecr.$AWS_Region.amazonaws.com/hdfc_ibpsportal:alpine-openjdk
 docker pull $AWS_AccountID.dkr.ecr.$AWS_Region.amazonaws.com/hdfc_ibps5serviceinstanceejb:sp2
