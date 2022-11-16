11aff47b15b32e33ffe0998eecc9c8a6cb648b15
Joslin.J <54833804+josliniyda27@users.noreply.github.com>
2022-11-16 11:49:00 +0530

    Update PullDockerImages.sh

--- PullDockerImages.sh
+++ PullDockerImages.sh
@@ -1,13 +1,20 @@
 set AWS_AccessKey=AKIAVFAJTEYIY7TQZFEE
+echo "AWS_AccessKey $AWS_AccessKey"
+
 set AWS_SecretKey=af+EgeLhdtaIv1r4IyOBP55F6TD8ZsXWG9ZhWf/M
+echo "AWS_SecretKey $AWS_SecretKey"
+
 set AWS_AccountID=354354931217
-set AWS_Region="ap-south-1"
+echo "AWS_AccountID $AWS_AccountID"
+
+set AWS_Region=ap-south-1
+echo "AWS_Region $AWS_Region"
 
 aws configure set aws_access_key_id $AWS_AccessKey
 aws configure set aws_secret_access_key $AWS_SecretKey
 aws configure set region $AWS_Region
 
-aws ecr get-login-password --region $AWS_Region | docker login --username AWS -p $(aws ecr get-login-password --region $AWS_region) $AWS_AccountID.dkr.ecr.$AWS_Region.amazonaws.com
+aws ecr get-login-password --region $AWS_Region$ | docker login --username AWS --password-stdin $AWS_AccountID$.dkr.ecr.$AWS_Region$.amazonaws.com
 
 docker pull $AWS_AccountID.dkr.ecr.$AWS_Region.amazonaws.com/hdfc_ibpsportal:alpine-openjdk
 docker pull $AWS_AccountID.dkr.ecr.$AWS_Region.amazonaws.com/hdfc_ibps5serviceinstanceejb:sp2
